f6a0d911a3b9ba3f3eb5f9082819fea6646f44b9
devlopersumit <sumitsj2002@gmail.com>
2026-09-12 19:44:37 +0530

    feat: add GitHub repo and new-issue URLs for site contributions

--- client/src/constants/brand.ts
+++ client/src/constants/brand.ts
@@ -4,3 +4,8 @@ export const TAGLINE =
 export const PRODUCT_NAME = 'IssueFinder'
 
 export const BUY_ME_A_COFFEE_URL = 'https://buymeacoffee.com/buildwithsumit'
+
+export const GITHUB_REPO_URL =
+  'https://github.com/sumitjhacodes/IssueFinder-Find-Beginner-Friendly-Issues'
+
+export const GITHUB_NEW_ISSUE_URL = `${GITHUB_REPO_URL}/issues/new`
